f728953fbc9bd343245ccd7c37950691d9b6e1a2
Petko Dimitrov <p.y.dimitrov01@gmail.com>
2025-04-11 10:10:08 +0300

    Added additional uc unit tests

--- uc/tasks_test.go
+++ uc/tasks_test.go
@@ -23,6 +23,27 @@ func getTask() domain.Task {
 	}
 }
 
+func getTasksList() []domain.Task {
+	return []domain.Task{
+		{
+			ID:          uuid.MustParse("1461ec84-ccff-4f3c-af34-65d0856ac3ce"),
+			Title:       "Do unit tests",
+			Description: "Create extensive unit tests for all layers",
+			Status:      "PENDING",
+			DueDate:     time.Date(2025, 4, 3, 0, 0, 0, 0, time.UTC),
+			CreatedAt:   time.Date(2025, 4, 3, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			ID:          uuid.MustParse("1461ec84-ccff-4f3c-af34-65d0856ac3dd"),
+			Title:       "Do other tests",
+			Description: "Create tests for all layers",
+			Status:      "PENDING",
+			DueDate:     time.Date(2025, 4, 3, 0, 0, 0, 0, time.UTC),
+			CreatedAt:   time.Date(2025, 4, 3, 0, 0, 0, 0, time.UTC),
+		},
+	}
+}
+
 func TestGetTaskById(t *testing.T) {
 	tests := []struct {
 		name           string
@@ -54,7 +75,7 @@ func TestGetTaskById(t *testing.T) {
 			},
 		},
 		{
-			name: "internal server error",
+			name: "error",
 			id:   "1461ec84-ccff-4f3c-af34-65d0856ac3ce",
 			repoMock: func(repoMock mock.MockTasksRepo) {
 				repoMock.EXPECT().GetTaskById(gomock.Any(), gomock.Eq(uuid.MustParse("1461ec84-ccff-4f3c-af34-65d0856ac3ce"))).Return(domain.Task{}, errors.New("not found"))
@@ -71,13 +92,110 @@ func TestGetTaskById(t *testing.T) {
 			defer ctrl.Finish()
 
 			repo := mock.NewMockTasksRepo(ctrl)
-			serivce := NewTasksService(repo)
+			service := NewTasksService(repo)
 
 			if tt.repoMock != nil {
 				tt.repoMock(*repo)
 			}
 
-			result, err := serivce.GetTaskById(context.Background(), uuid.MustParse(tt.id))
+			result, err := service.GetTaskById(context.Background(), uuid.MustParse(tt.id))
+			tt.checks(t, tt.expectedResult, result, err)
+		})
+	}
+}
+
+func TestGetTasks(t *testing.T) {
+	tests := []struct {
+		name           string
+		repoMock       func(repoMock mock.MockTasksRepo)
+		expectedResult []domain.Task
+		checks         func(t *testing.T, expected, result []domain.Task, err error)
+	}{
+		{
+			name: "happy path - OK",
+			repoMock: func(repoMock mock.MockTasksRepo) {
+				repoMock.EXPECT().GetTasks(gomock.Any()).Return(getTasksList(), nil)
+			},
+			expectedResult: getTasksList(),
+			checks: func(t *testing.T, expected, result []domain.Task, err error) {
+				require.NoError(t, err)
+				require.Equal(t, 2, len(result))
+			},
+		},
+		{
+			name: "error",
+			repoMock: func(repoMock mock.MockTasksRepo) {
+				repoMock.EXPECT().GetTasks(gomock.Any()).Return(nil, errors.New("not found"))
+			},
+			checks: func(t *testing.T, expected, result []domain.Task, err error) {
+				require.EqualError(t, err, "error fetching task: not found")
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			repo := mock.NewMockTasksRepo(ctrl)
+			service := NewTasksService(repo)
+
+			if tt.repoMock != nil {
+				tt.repoMock(*repo)
+			}
+
+			result, err := service.GetTasks(context.Background())
+			tt.checks(t, tt.expectedResult, result, err)
+		})
+	}
+}
+
+func TestCreateTask(t *testing.T) {
+	tests := []struct {
+		name           string
+		data           domain.Task
+		repoMock       func(repoMock mock.MockTasksRepo)
+		expectedResult domain.Task
+		checks         func(t *testing.T, expected, result domain.Task, err error)
+	}{
+		{
+			name: "happy path - OK",
+			data: getTask(),
+			repoMock: func(repoMock mock.MockTasksRepo) {
+				repoMock.EXPECT().CreateTask(gomock.Any(), gomock.Eq(getTask())).Return(getTask(), nil)
+			},
+			expectedResult: getTask(),
+			checks: func(t *testing.T, expected, result domain.Task, err error) {
+				require.NoError(t, err)
+				require.Equal(t, expected.ID, result.ID)
+			},
+		},
+		{
+			name: "error",
+			data: getTask(),
+			repoMock: func(repoMock mock.MockTasksRepo) {
+				repoMock.EXPECT().CreateTask(gomock.Any(), gomock.Eq(getTask())).Return(domain.Task{}, errors.New("not found"))
+			},
+			checks: func(t *testing.T, expected, result domain.Task, err error) {
+				require.EqualError(t, err, "error creating task: not found")
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			repo := mock.NewMockTasksRepo(ctrl)
+			service := NewTasksService(repo)
+
+			if tt.repoMock != nil {
+				tt.repoMock(*repo)
+			}
+
+			result, err := service.CreateTask(context.Background(), tt.data)
 			tt.checks(t, tt.expectedResult, result, err)
 		})
 	}
